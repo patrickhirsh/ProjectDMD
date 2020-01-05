915ebf5fe9e2ec6154825c1223cc35ce7f05ff7c
Patrick Hirsh <hirshpatrick@gmail.com>
2020-01-05 15:23:41 -0700

    Test application

---
+++ ProjectDMD/src/Test.cpp
@@ -0,0 +1,19 @@
+#include "../../Ember/src/Ember.h"
+
+class Test : public Ember::Application
+{
+public:
+	Test()
+	{
+
+	}
+	~Test()
+	{
+
+	}
+}
+
+Ember::Application* Ember::CreateApplication()
+{
+	return new Test();
+}
